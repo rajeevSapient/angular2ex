b8d38071a89196fff7057e3830d048a4dd7a50b3
rajeev roushan sharma <sharmaroushan@gmail.com>
2017-04-28 14:06:44 +0530

    property custom binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = "Hello world";
+  disabled = false;
+  btnlabel = "not disabled";
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.title = "new hello world";
+      this.btnlabel = "disabled now";
+      this.disabled = true;
+    }, 2000);
+  }
 }
