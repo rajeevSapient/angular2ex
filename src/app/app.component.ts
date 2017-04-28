af7e734fee713b191435f3f0e0bc34fe3762ef2d
rajeev roushan sharma <sharmaroushan@gmail.com>
2017-04-28 15:15:43 +0530

    live bindiing with event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  hobbies: string[] = [];
+  hobby: string;
 
-  constructor() { }
-
-  ngOnInit() { }
-
-  onSave(hobby) {
-    this.hobbies.push(hobby);
+  onChange(evt) {
+    this.hobby = evt.target.value;
   }
 
-  onRemove(index) {
-    this.hobbies.splice(index, 1);
-  }
 }
