53c1b3696b3d2bc359e7da3d486ac664f40a3e46
rajeev roushan sharma <sharmaroushan@gmail.com>
2017-04-30 00:08:10 +0530

    adding custom validator

--- src/app/bookform/bookform.component.ts
+++ src/app/bookform/bookform.component.ts
@@ -11,6 +11,8 @@ export class BookformComponent implements OnInit {
 
   @Output() newbook = new EventEmitter();
 
+  forbiddenTitle = ['html', 'css'];
+
   books: { title: string, author: string, releasedate: string, keyword?: string, imgsrc: string }[] = [];
   images = [
     '../assets/img/js1.png',
@@ -35,21 +37,28 @@ export class BookformComponent implements OnInit {
     let values = this.bookform.value;
     values.imgsrc = this.getRandomImage();
     this.books.push(values);
-    this.bookform.reset();
+    // this.bookform.reset();
     // this.newbook.emit(this.books);
-    console.log(this.books);
+    console.log(this.bookform);
   }
 
   ngOnInit() {
     this.bookform = new FormGroup({
       'bookdata': new FormGroup({
-        'title': new FormControl(null, [Validators.required]),
+        'title': new FormControl(null, [Validators.required, this.forbiddenTitleNames.bind(this)]),
         'author': new FormControl(null, [Validators.required]),
         'date': new FormControl(null)
       }),
       'authoremail': new FormControl(null, [Validators.required, Validators.email]),
       'keywords': new FormControl(null)
     });
+  }
+
+  forbiddenTitleNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenTitle.indexOf(control.value) !== -1) {
+      return { 'booknotallowed': true };
+    }
+    return null;
   }
 
 }
